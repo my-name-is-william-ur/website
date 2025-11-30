7bf837085510f8458be337e5d30bd0ee1aee00dd
junjie31415926 <jzhao58@u.rochester.edu>
2025-11-30 03:28:14 +0000

    Deploying to gh-pages from @ junjie31415926/website@71a68b2a37e564adb72866c712c70808d7f70627 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "my-name-is-william-ur/website",
+    "data-repo": "junjie31415926/website",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
